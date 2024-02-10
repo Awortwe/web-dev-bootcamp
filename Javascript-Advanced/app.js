50ffeae60f003cacf46761d756d0b1d761d29cfc
Awortwe <enockawor@gmail.com>
2024-02-09 18:12:16 -0800

    started with backend

---
+++ Javascript-Advanced/app.js
@@ -0,0 +1,10 @@
+// document.body.children[1].children[0].href = 'https://www.google.com';
+
+// DOM = Document Object Model
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com';
+
+anchorElement = document.querySelector('#external-id');
+// document.querySelectorAll('p a');
+anchorElement.href = 'https://google.com';
---
+++ Javascript-Basics/app.js
@@ -0,0 +1,32 @@
+let age = 32;
+let userName = 'Max';
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job = {
+    title: 'Developer',
+    place: 'New York',
+    salary: 50000
+};
+
+// alert(hobbies[0]);
+// alert(job.title);
+
+// let totalAdultYears;
+
+// function calculateAdultYears(userAge) {
+//     return userAge - 18; 
+// }
+
+// totalAdultYears = calculateAdultYears(40);
+// alert(totalAdultYears);
+
+// age = 45;
+// adultYears = age - 18;
+// alert(adultYears);
+
+let person = {
+    name: 'Max', // property
+    greet(){ // method
+        alert('Hello!');
+    }
+};
+
